1399527e49d1e628eab84e874d8a0d97cfcb17b7
patrikpordi <ppordi@umd.edu>
2023-11-15 19:19:52 -0500

    Fixing the launch file for logging

--- src/publisher_member_function.cpp
+++ src/publisher_member_function.cpp
@@ -1,15 +1,15 @@
-#include <chrono>
+#include <chrono> // This code uses <chrono>, which is part of the C++11 standard.
 #include <functional>
 #include <memory>
 #include <random>
 #include <string>
 
-
+#include "cpp_pubsub/srv/change_string.hpp"
 #include "rclcpp/rclcpp.hpp"
 #include "std_msgs/msg/string.hpp"
-#include "cpp_pubsub/srv/change_string.hpp"
 
-using namespace std::chrono_literals;
+// using namespace std::chrono_literals;
+using std::chrono_literals::operator""ms;
 
 /// @brief This class is used to publish a string message to a topic
 /// The class inherits from the rclcpp::Node class
@@ -21,16 +21,14 @@ using namespace std::chrono_literals;
 /// to be published
 class MinimalPublisher : public rclcpp::Node {
  public:
-  MinimalPublisher()
-      : Node("Custom_Node_Publisher"), msg_("Suppppp broooooo") {
+  MinimalPublisher() : Node("Custom_Node_Publisher"), msg_("Suppppp broooooo") {
     // Declare and acquire parameters from the parameter server
     this->declare_parameter("talker_f", 1.0);
-    
-    auto frequency =
-        this->get_parameter("talker_f");
-        
+
+    auto frequency = this->get_parameter("talker_f");
+
     if (frequency.get_type() == rclcpp::PARAMETER_NOT_SET) {
-      RCLCPP_ERROR_STREAM(rclcpp::get_logger("rclcpp"),
+      RCLCPP_ERROR_STREAM(this->get_logger(),
                           "Time period Parameter not set");
       throw std::runtime_error("Time period Parameter not set");
     }
@@ -42,7 +40,7 @@ class MinimalPublisher : public rclcpp::Node {
         "change_string",
         std::bind(&MinimalPublisher::change_string_callback, this,
                   std::placeholders::_1, std::placeholders::_2));
-    if (msg_ == "The power-ball number is = ") {
+    if (msg_ == "Suppppp broooooo") {
       RCLCPP_WARN_STREAM(this->get_logger(), "Using Default String");
     }
     // Create a timer
@@ -66,17 +64,14 @@ class MinimalPublisher : public rclcpp::Node {
   /// @param request
   /// @param response
   void change_string_callback(
-      const std::shared_ptr<cpp_pubsub::srv::ChangeString::Request>
-          request,
-      std::shared_ptr<cpp_pubsub::srv::ChangeString::Response>
-          response) {
+      const std::shared_ptr<cpp_pubsub::srv::ChangeString::Request> request,
+      std::shared_ptr<cpp_pubsub::srv::ChangeString::Response> response) {
     RCLCPP_DEBUG_STREAM(this->get_logger(), "Service Request Received");
     msg_ = request->new_string;
     response->success = true;
     RCLCPP_INFO_STREAM(this->get_logger(), ("Changing the string to: " + msg_));
-    
-    
   }
+
   rclcpp::TimerBase::SharedPtr timer_;
   rclcpp::Publisher<std_msgs::msg::String>::SharedPtr publisher_;
   rclcpp::Service<cpp_pubsub::srv::ChangeString>::SharedPtr service_;
@@ -89,7 +84,7 @@ class MinimalPublisher : public rclcpp::Node {
 int main(int argc, char *argv[]) {
   rclcpp::init(argc, argv);
   rclcpp::spin(std::make_shared<MinimalPublisher>());
-   // alert user about shutdown
+  // alert user about shutdown
   RCLCPP_FATAL_STREAM_ONCE(rclcpp::get_logger("rclpcpp"), "Subscriber closed");
   rclcpp::shutdown();
   return 0;
