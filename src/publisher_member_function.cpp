45f0d51a830826d26e99c62cf79baf89d7af291b
patrikpordi <ppordi@umd.edu>
2023-11-15 19:39:55 -0500

    RQT consol picture added

--- src/publisher_member_function.cpp
+++ src/publisher_member_function.cpp
@@ -1,3 +1,27 @@
+// Copyright 2016 Open Source Robotics Foundation, Inc.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+/**
+ * @file publisher_member_function.cpp
+ * @author Patrik Dominik Pördi (ppordi@umd.edu)
+ * @brief 
+ * @version 0.1
+ * @date 2023-11-15
+ * 
+ * @copyright Copyright (c) 2023
+ * 
+ */
 #include <chrono>  // NOLINT [build/c++11]
 #include <functional>  // This code uses <chrono>, which is part of the C++11 standard.
 #include <memory>
--- src/subscriber_member_function.cpp
+++ src/subscriber_member_function.cpp
@@ -12,6 +12,16 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
+/**
+ * @file subscriber_member_function.cpp
+ * @author Patrik Dominik Pördi (ppordi@umd.edu)
+ * @brief 
+ * @version 0.1
+ * @date 2023-11-15
+ * 
+ * @copyright Copyright (c) 2023
+ * 
+ */
 #include <functional>
 #include <memory>
 
